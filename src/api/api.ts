4d77284437f4735151a84d2bd9ba69fdb9993467
Evgeniy_Shabolin <steamguardemail@yandex.ru>
2022-10-11 16:28:20 +0300

    add style

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
 
+const appid = 'bde816a9134cfdfb0d91449aba12c8f'
+
 export const weatherApi = {
     getCurrentWeather() {
-        return axios.get('https://api.openweathermap.org/data/2.5/weather?lat=55.755826&lon=37.6173&appid=bde816a9134cfdfb0d91449aba12c8f8&units=metric')
+        return axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=55.800595&lon=37.473519&lang=ru&appid=${appid}8&units=metric`)
             .then(r => r.data)
     }
 }
--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,4 +1,3 @@
-
 import {ThunkDispatch} from '@reduxjs/toolkit'
 import {AppStateType} from '../redux/store'
 import {WeatherActions, weatherThunk} from '../redux/reducers/weatherReducer'
@@ -12,6 +11,17 @@ import {
     windSelector
 } from '../redux/selectors/selector'
 import {useDispatch, useSelector } from 'react-redux'
+import s from './weather.module.css'
+import cn from 'classnames'
+import {
+    ArrowDown,
+    ArrowDownLeft,
+    ArrowDownRight,
+    ArrowLeft,
+    ArrowRight,
+    ArrowUp,
+    ArrowUpLeft, ArrowUpRight
+} from 'react-bootstrap-icons'
 
 
 const Weather = () => {
@@ -39,28 +49,49 @@ const Weather = () => {
     }, [])
 
 
-
+    // s.grid-item s.grid-item-main
     return (
-        <div>
-            <div>
-                {city}
+        <div className={s.grid}>
+            <div className={cn(s.gridItem, s.gridItemMain)}>
+                <div className={cn(s.mainTemp, s.mainFont)}>
+                    {Math.round(mainTemp.temp)}°C
+                </div>
+                <div className={cn(s.mainCity, s.mainFont)}>
+                    {city}
+                </div>
+                <div className={cn(s.mainDescription, s.mainFont)}>
+                    На улице {weatherMain['0'].description}
+                </div>
+                <div className={cn(s.mainFeelLikes, s.mainFont)}>
+                    Ощущается как {Math.round(mainTemp.feels_like)}°C
+                </div>
+                <div className={cn(s.mainMaxMin, s.mainFont)}>
+                    {Math.round(mainTemp.temp_min)}°C/{Math.round(mainTemp.temp_max)}°C
+                </div>
             </div>
-            <div>
-                {mainTemp.temp_min}
+            <div className={cn(s.gridItem, s.gridItemAbout)}>
+                <div className={cn(s.windHeader)}>
+                    Ветер
+                </div>
+                <div className={cn(s.mainFont, s.windTxt)}>
+                    {wind.speed} м/c
+                </div>
+                <div className={cn(s.mainFont, s.windTxt)}>
+                    Направление ветра:  <span> </span>
+                    { wind.deg >= 25 && 340 <= wind.deg ? <span><ArrowLeft/> Западное</span> : null}
+                    { 26 <= wind.deg && wind.deg <= 70 ? <span><ArrowUpLeft/> Северо-Запад</span>  : null}
+                    { 71 <= wind.deg && wind.deg <= 115 ? <span><ArrowUp/> Северное</span>  : null}
+                    { 116 <= wind.deg && wind.deg <= 160 ? <span><ArrowUpRight/> Северо-Восточное</span>  : null}
+                    { 161 <= wind.deg && wind.deg <= 205 ? <span><ArrowRight/> Восточное</span>  : null}
+                    { 206 <= wind.deg && wind.deg <= 250 ? <span><ArrowDownRight/> Юго-Восточное </span>  : null}
+                    { 251 <= wind.deg && wind.deg <= 295 ? <span><ArrowDown/> Южное</span>  : null}
+                    { 296 <= wind.deg && wind.deg <= 339 ? <span><ArrowDownLeft /> Юго-Западное</span>  : null}
+                </div>
             </div>
-            <div>
-                {mainTemp.temp_max}
-            </div>
-            <div>
-                {mainTemp.feels_like}
-            </div>
-
-
-
-
-
+            <div className={cn(s.gridItem, s.gridItemCity)}>Город</div>
         </div>
     )
 }
+
 
 export default Weather
