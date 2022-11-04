f6aa0cf83267e1efb707dfa7fc21d8a133782657
Evgeniy_Shabolin <steamguardemail@yandex.ru>
2022-11-05 00:30:20 +0300

    fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,6 @@
 import axios from 'axios'
 import { WeatherState } from '../redux/reducers/weatherReducerSlice'
 
-const lat = 55.788320
-const lon = 37.484946
-const appid = 'bde816a9134cfdfb0d91449aba12c8f'
-
 export const weatherApi = {
     getCurrentWeather(cityName?: string) {
         return axios.get<WeatherState>(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=bde816a9134cfdfb0d91449aba12c8f8&lang=ru&units=metric`)
